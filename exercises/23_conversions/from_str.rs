601fffa21396f3826b70951ecf8d8de89b55c7ba
valentin morice <valentin@morice.live>
2026-07-19 18:15:47 +0200

    chore: update

--- exercises/23_conversions/from_str.rs
+++ exercises/23_conversions/from_str.rs
@@ -25,7 +25,7 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// TODO: Complete this `FromStr` implementation to be able to parse a `Person`
+// TOD(O: Complete this `FromStr` implementation to be able to parse a `Person`
 // out of a string in the form of "Mark,20".
 // Note that you'll need to parse the age component into a `u8` with something
 // like `"4".parse::<u8>()`.
@@ -41,7 +41,22 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {}
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut split = s.split(',');
+
+        let (Some(name), Some(age), None) = (split.next(), split.next(), split.next()) else {
+            return Err(ParsePersonError::BadLen)
+        };
+
+        if name.is_empty() {
+            return Err(ParsePersonError::NoName);
+        }
+
+        match age.parse() {
+            Ok(age) => Ok(Person { name: name.into(), age}),
+            Err(e) => Err(Self::Err::ParseInt(e))
+        }
+    }
 }
 
 fn main() {
